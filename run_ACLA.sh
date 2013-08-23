324e4b263ef980c78bb70c48c565a1397fd71a7d
Krishnan Vasudevan <naveneetha@yahoo.com>
2013-08-23 14:33:54 +0100

    Modified run_ACLA to output ambiguous fragment and its length too.

--- run_ACLA.sh
+++ run_ACLA.sh
@@ -27,24 +27,27 @@ run_randomcfg() {
             # first convert accent format to cfg format
             gacc="$grandom/$randomsize/$g.acc"
             _gcfg="$grandom/$randomsize/$g.cfg"    
-	    gcfg=$(mktemp ${_gcfg}.XXXXXX)
+	        gcfg=$(mktemp ${_gcfg}.XXXXXX)
             cat $gacc | egrep -v "^%nodefault|^%token" | sed -e "s/'/\"/g" -e 's/;$//' > $gcfg
             tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
             for token in $tokenlist
             do
                 sed -i -e "s/\b$token\b/\"$token\"/g" -e "s/'/\"/g" $gcfg
             done
-            sentence=$(timeout $timelimit $cmd -a $gcfg| egrep -o 'unambiguous\!|ambiguous string' | uniq)
-	    rm $gcfg
+            out=$(timeout $timelimit $cmd -a $gcfg| egrep 'unambiguous\!|ambiguous string')
+            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
+	        rm $gcfg
             ((cnt+=1))
-            if [ "$sentence" == "ambiguous string" ]
+            if [ "$amb" == "ambiguous string" ]
             then 
                 ((ambcnt+=1))
-                echo "$randomsize - $g,yes" | tee -a $result
+                ambstr=$(echo $out | awk -F': ' '{print $2}')
+                echo "$randomsize - $g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
                 continue
             fi
-            [ "$sentence" == "unambiguous!" ] && echo "$randomsize - $g,no" | tee -a $result  && continue
-            echo "$randomsize - $g," | tee -a $result
+            amb=$(echo $out | grep -o 'unambiguous\!')
+            [ "$amb" == "unambiguous!" ] && echo "$randomsize - $g,unambiguous,," | tee -a $result  && continue
+            echo "$randomsize - $g,,," | tee -a $result
         done
     done
     print_summary $ambcnt $cnt
@@ -63,32 +66,28 @@ run_lang() {
             gacc="$glang/acc/$g.$i.acc"
             _gcfg="$grammardir/cfg/$g.$i.cfg"
             [ ! -d $grammardir/cfg ] && mkdir -p $grammardir/cfg    
-	    gcfg=$(mktemp ${_gcfg}.XXXXXX)
-	    echo "$gcfg"
+	        gcfg=$(mktemp ${_gcfg}.XXXXXX)
+	        echo "$gcfg"
             cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " > $gcfg
             tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
             for token in $tokenlist
             do
                 sed -i -e "s/\b$token\b/\"$token\"/g" -e "s/'/\"/g" $gcfg
-#                if [ "${grammar}" == "Pascal" ]
-#                then 
-#                    sed -i -e 's/"UNSIGNED_INT"/"1"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"UNSIGNED_REAL"/"1.0"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"STRING"/"st"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"IDENTIFIER"/"id"/g' ${CFG_GRAMMAR_FILE}
-#                fi
             done            
-            sentence=$(timeout $timelimit $cmd -a $gcfg | egrep -o 'unambiguous\!|ambiguous string' | uniq)
-	    rm $gcfg
+            out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
+            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
+	        rm $gcfg
             ((cnt+=1))
-            if [ "$sentence" == "ambiguous string" ]
+            if [ "$amb" == "ambiguous string" ]
             then 
                 ((ambcnt+=1))
-                echo "$g.$i,yes" | tee -a $result
+                ambstr=$(echo $out | awk -F': ' '{print $2}')
+                echo "$g.$i,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
                 continue
             fi
-            [ "$sentence" == "unambiguous!" ] && echo "$g.$i,no" | tee -a $result  && continue
-            echo "$g.$i," | tee -a $result
+            amb=$(echo $out | grep -o 'unambiguous\!')
+            [ "$amb" == "unambiguous!" ] && echo "$g.$i,unambiguous,," | tee -a $result  && continue
+            echo "$g.$i,,," | tee -a $result
         done
     done
     print_summary $ambcnt $cnt
@@ -110,31 +109,27 @@ run_mutlang() {
              gacc="$gmutlang/acc/$type/$g.0_$n.acc"
              _gcfg="$gmutlang/cfg/$type/$g.0_$n.cfg"
              [ ! -d $gmutlang/cfg/$type ] && mkdir -p $gmutlang/cfg/$type
-	     gcfg=$(mktemp ${_gcfg}.XXXXXX)
+	         gcfg=$(mktemp ${_gcfg}.XXXXXX)
              cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " > $gcfg
              tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
              for token in $tokenlist
              do
                 sed -i -e "s/\b${token}\b/\"${token}\"/g" -e "s/'/\"/g" $gcfg
-#                if [ "${grammar}" == "Pascal" ]
-#                then 
-#                    sed -i -e 's/"UNSIGNED_INT"/"1"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"UNSIGNED_REAL"/"1.0"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"STRING"/"st"/g' ${CFG_GRAMMAR_FILE}
-#                    sed -i -e 's/"IDENTIFIER"/"id"/g' ${CFG_GRAMMAR_FILE}
-#                fi                
              done
-             sentence=$(timeout $timelimit $cmd -a $gcfg | egrep -o 'unambiguous\!|ambiguous string' | uniq)
-	     rm $gcfg
+             out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
+             amb=$(echo $out | grep -o 'ambiguous string' | uniq)
+	         rm $gcfg
              ((cnt+=1))
-             if [ "$sentence" == "ambiguous string" ]
+             if [ "$amb" == "ambiguous string" ]
              then
                  ((ambcnt+=1))
-                 echo "$g.0_$n,yes" | tee -a $result
+                 ambstr=$(echo $out | awk -F': ' '{print $2}')
+                 echo "$g.0_$n,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
                  continue
              fi
-             [ "$sentence" == "unambiguous!" ] && echo "$g.0_$n,no" | tee -a $result  && continue
-             echo "$g.0_$n," | tee -a $result            
+             amb=$(echo $out | grep -o 'unambiguous\!')
+             [ "$amb" == "unambiguous!" ] && echo "$g.0_$n,unambiguous,," | tee -a $result  && continue
+             echo "$g.0_$n,,," | tee -a $result            
           done
            done
            print_summary $ambcnt $cnt
@@ -153,24 +148,27 @@ run_boltzcfg() {
             # first convert accent format to cfg format
             gacc="$gboltz/$boltzsize/$g.acc"
             _gcfg="$gboltz/$boltzsize/$g.cfg"    
-	    gcfg=$(mktemp ${_gcfg}.XXXXXX)
+	        gcfg=$(mktemp ${_gcfg}.XXXXXX)
             cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " | sed -e 's/;$//g' > $gcfg
             tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
             for token in $tokenlist
             do
                sed -i -e "s/\b${token}\b/\"${token}\"/g" -e "s/'/\"/g" $gcfg
             done        
-            sentence=$(timeout $timelimit $cmd -a $gcfg| egrep -o 'unambiguous\!|ambiguous string' | uniq)
-	    rm $gcfg
+            out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
+            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
+	        rm $gcfg
             ((cnt+=1))
-            if [ "$sentence" == "ambiguous string" ]
+            if [ "$amb" == "ambiguous string" ]
             then 
                 ((ambcnt+=1))
-                echo "$boltzsize - $g,yes" | tee -a $result
+                ambstr=$(echo $out | awk -F': ' '{print $2}')
+                echo "$boltzsize - $g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
                 continue
             fi
-            [ "$sentence" == "unambiguous!" ] && echo "$boltzsize - $g,no" | tee -a $result  && continue
-            echo "$boltzsize - $g," | tee -a $result
+            amb=$(echo $out | grep -o 'unambiguous\!')
+            [ "$amb" == "unambiguous!" ] && echo "$boltzsize - $g,unambiguous,," | tee -a $result  && continue
+            echo "$boltzsize - $g,,," | tee -a $result
         done
     done
     print_summary $ambcnt $cnt
@@ -186,16 +184,20 @@ run_test() {
         gacc="$grammardir/test/$g/$g.acc"
         gcfg="$grammardir/test/$g/$g.cfg"    
         cat $gacc | egrep -v "^%nodefault|^;" | sed -e "s/'/\"/g" > $gcfg
-        sentence=$(timeout $timelimit $cmd -a $gcfg | egrep -o 'unambiguous\!|ambiguous string' | uniq)
+        out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
+        amb=$(echo $out | grep -o 'ambiguous string' | uniq)
+        rm $gcfg
         ((cnt+=1))
-        if [ "$sentence" == "ambiguous string" ]
+        if [ "$amb" == "ambiguous string" ]
         then
             ((ambcnt+=1))
-            echo "$g,yes" | tee -a $result
+            ambstr=$(echo $out | awk -F': ' '{print $2}')
+            echo "$g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
             continue
         fi
-        [ "$sentence" == "unambiguous!" ] && echo "$g,no" | tee -a $result  && continue
-        echo "$g," | tee -a $result
+        ambstr=$(echo $out | grep -o 'unambiguous\!')
+        [ "$ambstr" == "unambiguous!" ] && echo "$g,unambiguous,," | tee -a $result  && continue
+        echo "$g,,," | tee -a $result
     done
     print_summary $ambcnt $cnt
 }
