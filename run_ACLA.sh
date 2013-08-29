9652a7d0b3251762a56ae72f89c0af16fd329140
Krishnan Vasudevan <naveneetha@yahoo.com>
2013-08-29 14:55:55 +0100

    Refactored run_ACALA code for better printing of results

--- run_ACLA.sh
+++ run_ACLA.sh
@@ -15,11 +15,29 @@ print_summary() {
     echo -e "\nSummary: $summary \n--"
 }
 
+printrec() {
+    result="$1"
+    _out="$2"
+    _g="$3"
+    amb=$(echo $_out | grep -o 'ambiguous string' | uniq)
+    if [ "$amb" == "ambiguous string" ]
+    then 
+        ambstr=$(echo $out | awk -F': ' '{print $2}')
+        echo "$_g,yes,-,$ambstr,$(echo $ambstr | wc -c)" | tee -a $result
+    else
+        amb=$(echo $_out | grep -o 'unambiguous\!')
+        if [ "$amb" == "unambiguous!" ]
+        then
+            echo "$_g,unambiguous,,," | tee -a $result
+        else
+            echo "$_g,,,," | tee -a $result
+        fi    
+    fi
+}
+
 run_randomcfg() {
     result="$resultsdir/acla/$torun/$timelimit"
     cp /dev/null $result
-    ambcnt=0
-    cnt=0    
     for randomsize in $randomcfgsizes
     do
         for g in  $(seq 1 $nrandom)
@@ -35,29 +53,15 @@ run_randomcfg() {
                 sed -i -e "s/\b$token\b/\"$token\"/g" -e "s/'/\"/g" $gcfg
             done
             out=$(timeout $timelimit $cmd -a $gcfg| egrep 'unambiguous\!|ambiguous string')
-            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
-	        rm $gcfg
-            ((cnt+=1))
-            if [ "$amb" == "ambiguous string" ]
-            then 
-                ((ambcnt+=1))
-                ambstr=$(echo $out | awk -F': ' '{print $2}')
-                echo "$randomsize - $g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
-                continue
-            fi
-            amb=$(echo $out | grep -o 'unambiguous\!')
-            [ "$amb" == "unambiguous!" ] && echo "$randomsize - $g,unambiguous,," | tee -a $result  && continue
-            echo "$randomsize - $g,,," | tee -a $result
+            rm $gcfg
+            printrec "$result" "$out" "$randomsize - $g"
         done
     done
-    print_summary $ambcnt $cnt
 }
 
 run_lang() {
     result="$resultsdir/acla/$torun/$timelimit"
     cp /dev/null $result
-    ambcnt=0
-    cnt=0
     for g in $lgrammars
     do
         for i in $(seq 1 $nlang)
@@ -67,7 +71,6 @@ run_lang() {
             _gcfg="$grammardir/cfg/$g.$i.cfg"
             [ ! -d $grammardir/cfg ] && mkdir -p $grammardir/cfg    
 	        gcfg=$(mktemp ${_gcfg}.XXXXXX)
-	        echo "$gcfg"
             cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " > $gcfg
             tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
             for token in $tokenlist
@@ -75,72 +78,44 @@ run_lang() {
                 sed -i -e "s/\b$token\b/\"$token\"/g" -e "s/'/\"/g" $gcfg
             done            
             out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
-            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
 	        rm $gcfg
-            ((cnt+=1))
-            if [ "$amb" == "ambiguous string" ]
-            then 
-                ((ambcnt+=1))
-                ambstr=$(echo $out | awk -F': ' '{print $2}')
-                echo "$g.$i,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
-                continue
-            fi
-            amb=$(echo $out | grep -o 'unambiguous\!')
-            [ "$amb" == "unambiguous!" ] && echo "$g.$i,unambiguous,," | tee -a $result  && continue
-            echo "$g.$i,,," | tee -a $result
+            printrec "$result" "$out" "$g.$i" 
         done
     done
-    print_summary $ambcnt $cnt
 }
 
 run_mutlang() {
     for type in $mutypes
     do
-           result="$resultsdir/acla/$torun/${type}_${timelimit}"
-          cp /dev/null $result
-          ambcnt=0
-          cnt=0
-          echo "===> $type, result - $result"
-           for g in $mugrammars
-           do
-          for n in $(seq 1 $nmutations)
-          do
-             # convert grammar to cfg format
-             gacc="$gmutlang/acc/$type/$g.0_$n.acc"
-             _gcfg="$gmutlang/cfg/$type/$g.0_$n.cfg"
-             [ ! -d $gmutlang/cfg/$type ] && mkdir -p $gmutlang/cfg/$type
-	         gcfg=$(mktemp ${_gcfg}.XXXXXX)
-             cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " > $gcfg
-             tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
-             for token in $tokenlist
-             do
-                sed -i -e "s/\b${token}\b/\"${token}\"/g" -e "s/'/\"/g" $gcfg
-             done
-             out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
-             amb=$(echo $out | grep -o 'ambiguous string' | uniq)
-	         rm $gcfg
-             ((cnt+=1))
-             if [ "$amb" == "ambiguous string" ]
-             then
-                 ((ambcnt+=1))
-                 ambstr=$(echo $out | awk -F': ' '{print $2}')
-                 echo "$g.0_$n,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
-                 continue
-             fi
-             amb=$(echo $out | grep -o 'unambiguous\!')
-             [ "$amb" == "unambiguous!" ] && echo "$g.0_$n,unambiguous,," | tee -a $result  && continue
-             echo "$g.0_$n,,," | tee -a $result            
-          done
-           done
-           print_summary $ambcnt $cnt
+        result="$resultsdir/acla/$torun/${type}_${timelimit}"
+        cp /dev/null $result
+        echo "===> $type, result - $result"
+        for g in $mugrammars
+        do
+            for n in $(seq 1 $nmutations)
+            do
+                # convert grammar to cfg format
+                gacc="$gmutlang/acc/$type/$g.0_$n.acc"
+                _gcfg="$gmutlang/cfg/$type/$g.0_$n.cfg"
+                [ ! -d $gmutlang/cfg/$type ] && mkdir -p $gmutlang/cfg/$type
+	            gcfg=$(mktemp ${_gcfg}.XXXXXX)
+                cat $gacc | egrep -v "^\s*;|^%nodefault|^%token " > $gcfg
+                tokenlist=$(grep '%token' $gacc | sed -e 's/%token //' | tr -d ';,')
+                for token in $tokenlist
+                do
+                    sed -i -e "s/\b${token}\b/\"${token}\"/g" -e "s/'/\"/g" $gcfg
+                done
+                out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
+	            rm $gcfg
+	            printrec "$result" "$out" "$g.0_$n"
+            done
+        done
     done    
 }
 
 run_boltzcfg() {
     result="$resultsdir/acla/$torun/$timelimit"
     cp /dev/null $result
-    ambcnt=0
-    cnt=0
     for boltzsize in $boltzcfgsizes
     do    
         for g in  $(seq 1 $nboltz)
@@ -156,22 +131,10 @@ run_boltzcfg() {
                sed -i -e "s/\b${token}\b/\"${token}\"/g" -e "s/'/\"/g" $gcfg
             done        
             out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
-            amb=$(echo $out | grep -o 'ambiguous string' | uniq)
 	        rm $gcfg
-            ((cnt+=1))
-            if [ "$amb" == "ambiguous string" ]
-            then 
-                ((ambcnt+=1))
-                ambstr=$(echo $out | awk -F': ' '{print $2}')
-                echo "$boltzsize - $g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
-                continue
-            fi
-            amb=$(echo $out | grep -o 'unambiguous\!')
-            [ "$amb" == "unambiguous!" ] && echo "$boltzsize - $g,unambiguous,," | tee -a $result  && continue
-            echo "$boltzsize - $g,,," | tee -a $result
+            printrec "$result" "$out" "$boltzsize - $g"
         done
     done
-    print_summary $ambcnt $cnt
 }
 
 run_test() {
@@ -185,21 +148,9 @@ run_test() {
         gcfg="$grammardir/test/$g/$g.cfg"    
         cat $gacc | egrep -v "^%nodefault|^;" | sed -e "s/'/\"/g" > $gcfg
         out=$(timeout $timelimit $cmd -a $gcfg | egrep 'unambiguous\!|ambiguous string')
-        amb=$(echo $out | grep -o 'ambiguous string' | uniq)
         rm $gcfg
-        ((cnt+=1))
-        if [ "$amb" == "ambiguous string" ]
-        then
-            ((ambcnt+=1))
-            ambstr=$(echo $out | awk -F': ' '{print $2}')
-            echo "$g,yes,$ambstr,$(echo $ambstr | wc -w)" | tee -a $result
-            continue
-        fi
-        ambstr=$(echo $out | grep -o 'unambiguous\!')
-        [ "$ambstr" == "unambiguous!" ] && echo "$g,unambiguous,," | tee -a $result  && continue
-        echo "$g,,," | tee -a $result
+        printrec "$result" "$out" "$g"
     done
-    print_summary $ambcnt $cnt
 }
 
 main() {
