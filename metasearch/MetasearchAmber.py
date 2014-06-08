d5846144e2d7837b8a7de5ee09899fc270bdbf5e
Naveneetha Vasudevan <naveneetha@yahoo.com>
2014-06-08 23:51:30 +0100

    [metasearch][amber] Use standard deviation to determine the neighbour

--- metasearch/MetasearchAmber.py
+++ metasearch/MetasearchAmber.py
@@ -5,9 +5,12 @@ import getopt
 import math
 import MetaUtils
 
-TIMELIMIT = 30
-# we keep incrementing number of examples to search for by 10%
+TIMELIMIT = 10
 EXAMPLES_INCREMENT = 1.1
+EXAMPLES_INCREMENT_LVAL = 1.3
+EXAMPLES_INCREMENT_HVAL = 2.0
+LENGTH_LVAL = 3
+LENGTH_HVAL = 10
 
 class Hillclimb:
 
@@ -18,6 +21,7 @@ class Hillclimb:
         self.length = length
         self.ellipsis = ellipsis
         self.timelimit = timelimit
+        self.k_values = []
 
         amberx =  "%s/run_Amber.sh" % (self.expdir)
         self.cmd = [amberx,"-g",self.gset,"-t",str(self.timelimit)]
@@ -43,19 +47,57 @@ class Hillclimb:
         return MetaUtils.ambtotal(log)
 
 
+    def neighbour(self, neigh, sdev3neigh, sdev10neigh):
+        if len(self.k_values) < 3:
+            return  neigh
+
+        recent3 = self.k_values[-3:]
+        recent10 = self.k_values[-10:]
+        recent3vals,recent10vals = [],[]
+        for k,v in recent3:
+            recent3vals.append(v)
+        
+        for k,v in recent10:
+            recent10vals.append(v)
+        
+        sdev3 = MetaUtils.stddev(recent3vals)
+        sdev10 = MetaUtils.stddev(recent10vals)
+        
+        sys.stderr.write("recent10vals: %s, sdev3: %s, sdev10: %s \n" % (recent10vals,sdev3,sdev10))
+        if sdev3 < 3:
+            if sdev10 < 3:
+                return sdev10neigh
+            else:
+                return sdev3neigh
+        else:
+            return neigh
+
+
+    def neighbourlen(self, l):
+        return self.neighbour(l+1, l+3, l+10)
+        
+
+    def neighbourex(self, n):
+        return self.neighbour(int(math.ceil(n * EXAMPLES_INCREMENT)), 
+                  int(math.ceil(n * EXAMPLES_INCREMENT_LVAL)), 
+                  int(math.ceil(n * EXAMPLES_INCREMENT_HVAL))) 
+
 
     def run_examples(self):
         currex = self.examples
         amberoptcmd = ["-n",str(currex)]
         MetaUtils.runtool(self.cmd + amberoptcmd)
         currfit = self.fitness(currex)
+        self.k_values.append((currex,currfit))
 
         while True:
             sys.stderr.write("\n==> current examples count: %s, fitness: %s\n" % (str(currex),str(currfit)))
-            neighex = int(math.ceil(currex * EXAMPLES_INCREMENT))
+            neighex = self.neighbour(currex)
+            sys.stderr.write("neighex: %s" % (str(neighex)))
             amberoptcmd = ["-n",str(neighex)]
             MetaUtils.runtool(self.cmd + amberoptcmd)
             newfit = self.fitness(neighex)
+            self.k_values.append((neighex,newfit))            
             
             sys.stderr.write("new fitness: %s " % str(newfit))
 
@@ -72,14 +114,16 @@ class Hillclimb:
         amberoptcmd = ["-l",str(currlen)]
         MetaUtils.runtool(self.cmd + amberoptcmd)
         currfit = self.fitness(currlen)
+        self.k_values.append((currlen,currfit))
 
         while True:
             sys.stderr.write("\n==> current length: %s, fitness: %s\n" % (str(currlen),str(currfit)))
-            neighlen = currlen + 1
+            neighlen = self.neighbourlen(currlen)
             amberoptcmd = ["-l",str(neighlen)]
             MetaUtils.runtool(self.cmd + amberoptcmd)
             newfit = self.fitness(neighlen)
-            
+            self.k_values.append((neighlen,newfit))            
+
             sys.stderr.write("new fitness: %s " % str(newfit))
 
             if newfit >= currfit:
@@ -87,6 +131,8 @@ class Hillclimb:
                 currfit = newfit
             else:
                 sys.stderr.write("\n** Reached maxima! length: %s ambiguities found: %s **\n" % (str(currlen), str(currfit)))
+                for k,v in self.k_values:
+                    print "%s,%s" % (k,v)
                 sys.exit(0) 
 
 
@@ -119,6 +165,9 @@ if __name__ == "__main__":
             ellipsis = True
         else: 
             usage("Unknown argument '%s'" % opt[0])   
+
+    if (examples is None) and (length is None):
+        usage()
 
     Hillclimb(expdir, gset, examples, length, ellipsis, TIMELIMIT)
     
