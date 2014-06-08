398e1227ff0dfccc29edef865212eda46253799b
Naveneetha Vasudevan <naveneetha@yahoo.com>
2014-06-08 23:52:09 +0100

    [metasearch][amber] Use standard deviation to determine the neighbour

--- metasearch/MetasearchAmbiDexter.py
+++ metasearch/MetasearchAmbiDexter.py
@@ -5,7 +5,7 @@ import getopt
 import math
 import MetaUtils
 
-TIMELIMIT = 30
+TIMELIMIT = 10
 
 class Hillclimb:
 
@@ -25,6 +25,7 @@ class Hillclimb:
         else:
             self.logdirprefix = "%ss" % (self.timelimit)
 
+        self.k_values = []
         self.run()
 
 
@@ -35,18 +36,50 @@ class Hillclimb:
         return MetaUtils.ambtotal(log)
 
 
+
+    def neighbour(self, l):
+        """ if the last 3 values are roughly the same, the return +3 
+            otherwise, + 1"""
+        if len(self.k_values) < 3:
+            return l + 1
+
+        recent3 = self.k_values[-3:]
+        recent10 = self.k_values[-10:]
+        recent3vals,recent10vals = [],[]
+        for k,v in recent3:
+            recent3vals.append(v)
+        
+        for k,v in recent10:
+            recent10vals.append(v)
+        
+        sdev3 = MetaUtils.stddev(recent3vals)
+        sdev10 = MetaUtils.stddev(recent10vals)
+        
+        sys.stderr.write("recent10vals: %s, sdev3: %s, sdev10: %s \n" % (recent10vals,sdev3,sdev10))
+        if sdev3 < 3:
+            if sdev10 < 3:
+                return l + 10
+            else:
+                return l + 3
+        else:
+            return l + 1
+
+
+
     def run(self):
         currlen = self.length
         ambidextoptcmd = ["-k",str(currlen)]
         MetaUtils.runtool(self.cmd + ambidextoptcmd)
         currfit = self.fitness(currlen)
+        self.k_values.append((currlen,currfit))
 
         while True:
             sys.stderr.write("\n==> current length: %s, fitness: %s\n" % (str(currlen),str(currfit)))
-            neighlen = currlen + 1
+            neighlen = self.neighbour(currlen)
             ambidextoptcmd = ["-k",str(neighlen)]
             MetaUtils.runtool(self.cmd + ambidextoptcmd)
             newfit = self.fitness(neighlen)
+            self.k_values.append((neighlen,newfit))
             
             sys.stderr.write("new fitness: %s " % str(newfit))
 
@@ -55,6 +88,8 @@ class Hillclimb:
                 currfit = newfit
             else:
                 sys.stderr.write("\n** Reached maxima! length: %s ambiguities found: %s **\n" % (str(currlen), str(currfit)))
+                for k,v in self.k_values:
+                    print "%s,%s" % (k,v)
                 sys.exit(0) 
 
 
