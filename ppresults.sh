69f2f4500bdc64b307877be07bbea8b6a3d8bdb2
krish <krish.vasudevan@polestarglobal.com>
2015-02-21 18:05:17 +0000

    Fix ppresults

--- ppresults.sh
+++ ppresults.sh
@@ -5,9 +5,12 @@ pp_acla() {
     aclarow=""
     for timelimit in $timelimits
     do
-        result="$resultsdir/acla/$g/${timelimit}s"
-        [ ! -z "$2" ] && result="$resultsdir/acla/$g/${2}_${timelimit}s"
-        n_amb_acla=$(grep -c "yes" $result)
+        if [ ! -z "$2" ]; then
+            type="$2"
+            n_amb_acla=$(cat $resultsdir/acla/$g/${timelimit}s/$type/*/log | grep -c yes)
+        else
+            n_amb_acla=$(cat $resultsdir/acla/$g/${timelimit}s/log | grep -c yes)
+        fi
         aclarow="$aclarow | ($timelimit,$n_amb_acla) "
     done
     printf '%-20s %-200s\n' "ACLA [$g ${2}]" "$aclarow"
@@ -21,12 +24,12 @@ pp_amber_ex() {
         amberellexrow=""
         for timelimit in $timelimits
         do
-            result="$resultsdir/amber/$g/${timelimit}s_`echo "examples $amberex" | sed -e 's/\s/_/g'`"
-            [ ! -z "$2" ] && result="$resultsdir/amber/$g/${2}_${timelimit}s_`echo "examples $amberex" | sed -e 's/\s/_/g'`"
-            result_ell="$resultsdir/amber/$g/${timelimit}s_`echo "ellipsis examples $amberex" | sed -e 's/\s/_/g'`"
-            [ ! -z "$2" ] && result_ell="$resultsdir/amber/$g/${2}_${timelimit}s_`echo "ellipsis examples $amberex" | sed -e 's/\s/_/g'`"
-            n_amb_amber_ex=$(grep -c "yes" $result)
-            n_amb_amber_ell_ex=$(grep -c "yes" $result_ell)
+            result="$resultsdir/amber/$g/${timelimit}s_$(echo "examples $amberex" | sed -e 's/\s/_/g')/log"
+            [ ! -z "$2" ] && result="$resultsdir/amber/$g/${timelimit}s_$(echo "examples $amberex" | sed -e 's/\s/_/g')/$2/*/log"
+            result_ell="$resultsdir/amber/$g/${timelimit}s_$(echo "examples $amberex ellipsis" | sed -e 's/\s/_/g')/log"
+            [ ! -z "$2" ] && result_ell="$resultsdir/amber/$g/${timelimit}s_$(echo "examples $amberex ellipsis" | sed -e 's/\s/_/g')/$2/*/log"
+            n_amb_amber_ex=$(cat $result | grep -c "yes")
+            n_amb_amber_ell_ex=$(cat $result_ell | grep -c "yes")
             amberexrow="$amberexrow | ($timelimit,$n_amb_amber_ex)"
             amberellexrow="$amberellexrow | ($timelimit,$n_amb_amber_ell_ex)"
         done
@@ -43,12 +46,12 @@ pp_amber_len() {
         amberelllenrow=""
         for timelimit in $timelimits
         do
-            result="$resultsdir/amber/$g/${timelimit}s_`echo "length $amberlen" | sed -e 's/\s/_/g'`"
-            [ ! -z "$2" ] && result="$resultsdir/amber/$g/${2}_${timelimit}s_`echo "length $amberlen" | sed -e 's/\s/_/g'`"
-            result_ell="$resultsdir/amber/$g/${timelimit}s_`echo "ellipsis length $amberlen" | sed -e 's/\s/_/g'`"
-            [ ! -z "$2" ] && result_ell="$resultsdir/amber/$g/${2}_${timelimit}s_`echo "ellipsis length $amberlen" | sed -e 's/\s/_/g'`"
-            n_amb_amber_len=$(grep -c "yes" $result)
-            n_amb_amber_ell_len=$(grep -c "yes" $result_ell)
+            result="$resultsdir/amber/$g/${timelimit}s_$(echo "length $amberlen" | sed -e 's/\s/_/g')/log"
+            [ ! -z "$2" ] && result="$resultsdir/amber/$g/${timelimit}s_$(echo "length $amberlen" | sed -e 's/\s/_/g')/$2/*/log"
+            result_ell="$resultsdir/amber/$g/${timelimit}s_$(echo "length $amberlen ellipsis" | sed -e 's/\s/_/g')/log"
+            [ ! -z "$2" ] && result_ell="$resultsdir/amber/$g/${timelimit}s_$(echo "length $amberlen ellipsis" | sed -e 's/\s/_/g')/$2/*/log"
+            n_amb_amber_len=$(cat $result | grep -c "yes")
+            n_amb_amber_ell_len=$(cat $result_ell | grep -c "yes")
             amberlenrow="$amberlenrow | ($timelimit,$n_amb_amber_len)"
             amberelllenrow="$amberelllenrow | ($timelimit,$n_amb_amber_ell_len)"
         done
@@ -63,33 +66,33 @@ pp_ambidexter_len() {
     do
         frow=""
         slr1frow=""
-	lr0frow=""
-	lr1frow=""
+        lr0frow=""
+        lr1frow=""
         lalr1frow=""
-        koptions="-q_-pg_-k_${ambilen}"
+        koptions="-k_${ambilen}"
         for timelimit in $timelimits
         do
-            result="$resultsdir/ambidexter/$g/${timelimit}s_f_${memlimit}_${koptions}"
-            [ ! -z "$2" ] && result="$resultsdir/ambidexter/$g/${2}_${timelimit}s_f_${memlimit}_${koptions}"
-            n_amb_ambidexter_f_k=$(grep -c "yes" $result)
+            result="$resultsdir/ambidexter/$g/${timelimit}s_${koptions}/log"
+            [ ! -z "$2" ] && result="$resultsdir/ambidexter/$g/${timelimit}s_${koptions}/$2/*/log"
+            n_amb_ambidexter_k=$(cat $result | grep -c "yes")
 
-            result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_slr1f_${memlimit}_${koptions}"
-            [ ! -z "$2" ] && result_slr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_slr1f_${memlimit}_${koptions}"
-            n_amb_ambidexter_slr1f_k=$(grep -c "yes" $result_slr1)
+            result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_slr1_${koptions}/log"
+            [ ! -z "$2" ] && result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_slr1_${koptions}/$2/*/log"
+            n_amb_ambidexter_slr1f_k=$(cat $result_slr1 | grep -c "yes")
 
-            result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_lr0f_${memlimit}_${koptions}"
-            [ ! -z "$2" ] && result_lr0="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lr0f_${memlimit}_${koptions}"
-            n_amb_ambidexter_lr0f_k=$(grep -c "yes" $result_lr0)
+            result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr0_${koptions}/log"
+            [ ! -z "$2" ] && result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr0_${koptions}/$2/*/log"
+            n_amb_ambidexter_lr0f_k=$(cat $result_lr0 | grep -c "yes")
 
-            result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_lr1f_${memlimit}_${koptions}"
-            [ ! -z "$2" ] && result_lr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lr1f_${memlimit}_${koptions}"
-            n_amb_ambidexter_lr1f_k=$(grep -c "yes" $result_lr1)
+            result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr1_${koptions}/log"
+            [ ! -z "$2" ] && result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr1_${koptions}/$2/*/log"
+            n_amb_ambidexter_lr1f_k=$(cat $result_lr1 | grep -c "yes")
 
-            result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_lalr1f_${memlimit}_${koptions}"
-            [ ! -z "$2" ] && result_lalr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lalr1f_${memlimit}_${koptions}"
-            n_amb_ambidexter_lalr1f_k=$(grep -c "yes" $result_lalr1)
+            result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lalr1_${koptions}/log"
+            [ ! -z "$2" ] && result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lalr1_${koptions}/$2/*/log"
+            n_amb_ambidexter_lalr1f_k=$(cat $result_lalr1 | grep -c "yes")
 
-            frow="$frow | ($timelimit,$n_amb_ambidexter_f_k)"
+            frow="$frow | ($timelimit,$n_amb_ambidexter_k)"
             slr1frow="$slr1frow | ($timelimit,$n_amb_ambidexter_slr1f_k)"
             lr0frow="$lr0frow | ($timelimit,$n_amb_ambidexter_lr0f_k)"
             lr1frow="$lr1frow | ($timelimit,$n_amb_ambidexter_lr1f_k)"
@@ -105,72 +108,100 @@ pp_ambidexter_len() {
 
 pp_ambidexter_inclen() {
     g="$1"
-    ik_frow=""
-    ik_slr1frow=""
-    ik_lr0frow=""
-    ik_lr1frow=""
-    ik_lalr1frow=""
-    ikoptions="-q_-pg_-ik_0"
+    i_frow=""
+    i_slr1frow=""
+    i_lr0frow=""
+    i_lr1frow=""
+    i_lalr1frow=""
+    ioptions="-i_0"
     for timelimit in $timelimits
     do
-        result="$resultsdir/ambidexter/$g/${timelimit}s_f_${memlimit}_${ikoptions}"
-        [ ! -z "$2" ] && result="$resultsdir/ambidexter/$g/${2}_${timelimit}s_f_${memlimit}_${ikoptions}"
-        n_amb_ambidexter_f_ik=$(grep -c "yes" $result)
+        result="$resultsdir/ambidexter/$g/${timelimit}s_${ioptions}/log"
+        [ ! -z "$2" ] && result="$resultsdir/ambidexter/$g/${timelimit}s_${ioptions}/$2/*/log"
+        n_amb_ambidexter_i=$(cat $result | grep -c "yes")
+        echo ""
 
-        result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_slr1f_${memlimit}_${ikoptions}"
-        [ ! -z "$2" ] && result_slr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_slr1f_${memlimit}_${ikoptions}"
-        n_amb_ambidexter_slr1f_ik=$(grep -c "yes" $result_slr1)
+        result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_slr1_${ioptions}/log"
+        [ ! -z "$2" ] && result_slr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_slr1_${ioptions}/$2/*/log"
+        n_amb_ambidexter_slr1f_i=$(cat $result_slr1 | grep -c "yes")
 
-        result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_lr0f_${memlimit}_${ikoptions}"
-        [ ! -z "$2" ] && result_lr0="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lr0f_${memlimit}_${ikoptions}"
-        n_amb_ambidexter_lr0f_ik=$(grep -c "yes" $result_lr0)
+        result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr0_${ioptions}/log"
+        [ ! -z "$2" ] && result_lr0="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr0_${ioptions}/$2/*/log"
+        n_amb_ambidexter_lr0f_i=$(cat $result_lr0 | grep -c "yes")
 
-        result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_lr1f_${memlimit}_${ikoptions}"
-        [ ! -z "$2" ] && result_lr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lr1f_${memlimit}_${ikoptions}"
-        n_amb_ambidexter_lr1f_ik=$(grep -c "yes" $result_lr1)
+        result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr1_${ioptions}/log"
+        [ ! -z "$2" ] && result_lr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lr1_${ioptions}/$2/*/log"
+        n_amb_ambidexter_lr1f_i=$(cat $result_lr1 | grep -c "yes")
 
-        result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_lalr1f_${memlimit}_${ikoptions}"
-        [ ! -z "$2" ] && result_lalr1="$resultsdir/ambidexter/$g/${2}_${timelimit}s_lalr1f_${memlimit}_${ikoptions}"
-        n_amb_ambidexter_lalr1f_ik=$(grep -c "yes" $result_lalr1)
+        result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lalr1_${ioptions}/log"
+        [ ! -z "$2" ] && result_lalr1="$resultsdir/ambidexter/$g/${timelimit}s_-f_lalr1_${ioptions}/$2/*/log"
+        n_amb_ambidexter_lalr1f_i=$(cat $result_lalr1 | grep -c "yes")
 
-        ik_frow="$ik_frow | ($timelimit,$n_amb_ambidexter_f_ik)"
-        ik_slr1frow="$ik_slr1frow | ($timelimit,$n_amb_ambidexter_slr1f_ik)"
-        ik_lr0frow="$ik_lr0frow | ($timelimit,$n_amb_ambidexter_lr0f_ik)"
-        ik_lr1frow="$ik_lr1frow | ($timelimit,$n_amb_ambidexter_lr1f_ik)"
-        ik_lalr1frow="$ik_lalr1frow | ($timelimit,$n_amb_ambidexter_lalr1f_ik)"            
+        i_frow="$i_frow | ($timelimit,$n_amb_ambidexter_i)"
+        i_slr1frow="$i_slr1frow | ($timelimit,$n_amb_ambidexter_slr1f_i)"
+        i_lr0frow="$i_lr0frow | ($timelimit,$n_amb_ambidexter_lr0f_i)"
+        i_lr1frow="$i_lr1frow | ($timelimit,$n_amb_ambidexter_lr1f_i)"
+        i_lalr1frow="$i_lalr1frow | ($timelimit,$n_amb_ambidexter_lalr1f_i)"            
     done
-    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: n/a)" "$ik_frow"
-    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: slr1)" "$ik_slr1frow"
-    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lr0)" "$ik_lr0frow"
-    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lr1)" "$ik_slr1frow"
-    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lalr1)" "$ik_lalr1frow"
+    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: n/a)"   "$i_frow"
+    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: slr1)"  "$i_slr1frow"
+    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lr0)"   "$i_lr0frow"
+    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lr1)"   "$i_slr1frow"
+    printf '%-20s %-200s\n' "AmbiDexter [$g ${2}] (ik, filter: lalr1)" "$i_lalr1frow"
 }
 
 pp_sinbad() {
     g="$1"
+    type=""
+    [ ! -z "$2" ] && type="$2"
+    wgt=""
+    [ ! -z "$3" ] && wgt="$3"
     prrow=""
     for timelimit in $timelimits
     do
-        result="$resultsdir/sinbad/$g/purerandom_${timelimit}s"
-        [ ! -z "$2" ] && result="$resultsdir/sinbad/$g/${2}_purerandom_${timelimit}s"
-        n_amb_sinbad_purerandom=$(grep -c "yes" $result)
+        result="$resultsdir/sinbad/$g/${timelimit}s_-b_purerandom/log"
+        [ ! -z "$type" ] && result="$resultsdir/sinbad/$g/${timelimit}s_-b_purerandom/$type/*/log"
+        n_amb_sinbad_purerandom=$(cat $result | grep -c "yes")
         prrow="$prrow | ($timelimit,$n_amb_sinbad_purerandom)"
     done
     printf '%-20s %-200s\n' "sinbad [$g ${2}] [purerandom]" "$prrow"
 
-    
-    for d in $Tdepths
+    for dyn in $backends
     do
-        _options="-d_$d"
-        dyn1row=""
-        for timelimit in $timelimits
+        for d in $Tdepths
         do
-            result_dyn="$resultsdir/sinbad/$g/dynamic1_${timelimit}s_${_options}"
-            [ ! -z "$2" ] && result_dyn="$resultsdir/sinbad/$g/${2}_dynamic1_${timelimit}s_${_options}"
-            n_amb_sinbad_dyn=$(grep -c "yes" $result_dyn)
-            dyn1row="$dyn1row | ($timelimit,$n_amb_sinbad_dyn)"
+            d_options="-d_$d"
+            prrow=""
+            for timelimit in $timelimits
+            do
+                result="$resultsdir/sinbad/$g/${timelimit}s_-b_${dyn}_${d_options}/log"
+                [ ! -z "$type" ] && result="$resultsdir/sinbad/$g/${timelimit}s_-b_${dyn}_${d_options}/$type/*/log"
+                n_amb_sinbad_dyn=$(cat $result | grep -c "yes")
+                prrow="$prrow | ($timelimit,$n_amb_sinbad_dyn)"
+            done
+            printf '%-20s %-200s\n' "sinbad [$g ${2}] [$dyn d=$d]" "$prrow"
         done
-        printf '%-20s %-200s\n' "sinbad [$g ${2}] [dynamic1 d=$d]" "$dyn1row"
+    done
+
+    for dyn in $wgtbackends
+    do
+        for d in $Tdepths
+        do
+            d_options="-d_$d"
+            for w in $weights
+            do
+                w_options="-w_$w"
+                prrow=""
+                for timelimit in $timelimits
+                do
+                    result="$resultsdir/sinbad/$g/${timelimit}s_-b_${dyn}_${d_options}_${w_options}/log"
+                    [ ! -z "$type" ] && result="$resultsdir/sinbad/$g/${timelimit}s_-b_${dyn}_${d_options}_${w_options}/$type/*/log"
+                    n_amb_sinbad_dyn=$(cat $result | grep -c "yes")
+                    prrow="$prrow | ($timelimit,$n_amb_sinbad_dyn)"
+                done
+                printf '%-20s %-200s\n' "sinbad [$g ${2}] [$dyn d=$d w=$w]" "$prrow"
+            done
+        done
     done
 }
 
@@ -214,28 +245,28 @@ do
     ##########  AMBIDEXTER  ########### 
 
     ##### LENGTH #####
-    if [ "$g" == "mutlang" ]
-    then
-        for type in $mutypes
-        do
-            pp_ambidexter_len $g $type
-         done
-    else
-        pp_ambidexter_len $g
-    fi
-
-    ##### INCREMENTAL LENGTH #####
-    if [ "$g" == "mutlang" ]
-    then
-        for type in $mutypes
-        do
-            pp_ambidexter_inclen $g $type
+   if [ "$g" == "mutlang" ]
+   then
+       for type in $mutypes
+       do
+           pp_ambidexter_len $g $type
         done
-    else
-        pp_ambidexter_inclen $g
-    fi
+   else
+       pp_ambidexter_len $g
+   fi
+
+   ##### INCREMENTAL LENGTH #####
+    if [ "$g" == "mutlang" ]
+   then
+       for type in $mutypes
+       do
+           pp_ambidexter_inclen $g $type
+       done
+   else
+       pp_ambidexter_inclen $g
+   fi
                 
- 
+
     ##########  SINBAD ########### 
     if [ "$g" == "mutlang" ]
     then
