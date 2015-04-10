d870925ddd4da54dfc2b0f474a1b7b5dbe5291c7
krish <krish.vasudevan@polestarglobal.com>
2015-04-10 17:16:23 +0100

    Bit of refactoring - use the common functions from MetaUtils

--- metasearch/MetasearchAmbiDexter.py
+++ metasearch/MetasearchAmbiDexter.py
@@ -6,117 +6,55 @@ import math
 import MetaUtils
 
 TIMELIMIT = 10
+LEN_STEP1 = 1
+LEN_STEP2 = 3
+LEN_STEP3 = 5
 
 class Hillclimb:
 
     def __init__(self, expdir, gset, length, filter, timelimit):
-        self.expdir = expdir
-        self.gset = gset
         self.length = length
-        self.filter = filter
-        self.timelimit = timelimit
+        self.logd = "%s/results/ambidexter/%s/%ss" % (expdir, gset, timelimit)
 
-        ambidextx =  "%s/run_AmbiDexter.sh" % (self.expdir)
-        self.cmd = [ambidextx,"-g",self.gset,"-t",str(self.timelimit)]
+        ambidextx =  "%s/run_AmbiDexter.sh" % (expdir)
+        self.cmd = [ambidextx, "-g", gset, "-t", str(timelimit)]
 
-        if self.filter is not None:
-            self.cmd += ["-f",self.filter]
-            self.logdirprefix = "%ss_-f_%s" % (self.timelimit,self.filter)
-        else:
-            self.logdirprefix = "%ss" % (self.timelimit)
+        if filter is not None:
+            self.cmd += ["-f", filter]
+            self.logd += "_-f_%s" % (filter)
 
+        self.logd += "_-k"
         self.k_values = []
         self.run()
 
 
-    def fitness(self, optval):
-        """ fitness -> number of ambiguities found """
-        logdir = "%s_-k_%s" % (self.logdirprefix, optval)
-        log =  "%s/results/%s/%s/%s/log" % (self.expdir, "ambidexter", self.gset, logdir) 
-        return MetaUtils.ambtotal(log)
-
-
-
-    def neighbour(self, l):
-        """ if the last 3 values are roughly the same, the return +3 
-            otherwise, + 1"""
-        if len(self.k_values) < 3:
-            return l + 1
-
-        recent3 = self.k_values[-3:]
-        recent10 = self.k_values[-10:]
-        recent3vals,recent10vals = [],[]
-        for k,v in recent3:
-            recent3vals.append(v)
-        
-        for k,v in recent10:
-            recent10vals.append(v)
-        
-        sdev3 = MetaUtils.stddev(recent3vals)
-        sdev10 = MetaUtils.stddev(recent10vals)
-        
-        sys.stderr.write("recent10vals: %s, sdev3: %s, sdev10: %s \n" % (recent10vals,sdev3,sdev10))
-        if sdev3 < 3:
-            if sdev10 < 3:
-                return l + 10
-            else:
-                return l + 3
-        else:
-            return l + 1
-
-
-    def keep_running(self):
-        """ if we see a downward trend for the last 3 values, stop """
-        if len(self.k_values) > 3:
-            _, fit = self.k_values[-1]
-            _, _fit = self.k_values[-2]
-            _, __fit = self.k_values[-3]
-            _, ___fit = self.k_values[-4]
-            if (fit <= _fit) and (_fit <= __fit) and (__fit <= ___fit):
-                return False
-
-        return True
-
-
-    def finish(self):
-        fitvals = [f for k,f in self.k_values]
-        fitkeys = []
-        for k,f in self.k_values:
-            if f == max(fitvals):
-                fitkeys.append(k)
-
-        print "\n** Reached (local) maxima! **\n"
-        for k,f in self.k_values:
-            print "(%s,%s)" % (k,f)
-
-        msg = "Options %s found %s ambiguities **\n" % (fitkeys,max(fitvals))
-        sys.stderr.write(msg)
-        sys.exit(0) 
-
+    def neighbour(self, v):
+        return MetaUtils.neighbour(self.k_values,
+                                   v+LEN_STEP1,
+                                   v+LEN_STEP2,
+                                   v+LEN_STEP3)
 
 
     def run(self):
         currlen = self.length
         ambidextoptcmd = ["-k",str(currlen)]
         MetaUtils.runtool(self.cmd + ambidextoptcmd)
-        currfit = self.fitness(currlen)
+        currfit = MetaUtils.fitness("%s_%s/log" % (self.logd, currlen))
         self.k_values.append((currlen,currfit))
 
         while True:
-            sys.stderr.write("\n==> current length: %s, fitness: %s\n" % (str(currlen),str(currfit)))
+            sys.stderr.write("\n==> current length: %s, fitness: %s\n" \
+                            % (str(currlen),str(currfit)))
             neighlen = self.neighbour(currlen)
             ambidextoptcmd = ["-k",str(neighlen)]
             MetaUtils.runtool(self.cmd + ambidextoptcmd)
-            newfit = self.fitness(neighlen)
-            self.k_values.append((neighlen,newfit))
-            
+            newfit = MetaUtils.fitness("%s_%s/log" % (self.logd, neighlen))
             sys.stderr.write("new fitness: %s " % str(newfit))
+            self.k_values.append((neighlen,newfit))
 
-            if self.keep_running():
+            if MetaUtils.keep_running(self.k_values):
                 currlen = neighlen
                 currfit = newfit
-            else:
-                self.finish()
 
 
 def usage(msg=None):
