d1ce4d50678f91a6d21027be316aa1b265ac3a89
krish <krish.vasudevan@polestarglobal.com>
2015-04-10 15:50:03 +0100

    Lots of refactoring. Essentially all the common functions have been moved to MetaUtils

--- metasearch/MetaUtils.py
+++ metasearch/MetaUtils.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import  os, subprocess, sys
+import os, subprocess, sys
 import math
 
 def write(msg):
@@ -8,16 +8,23 @@ def write(msg):
     sys.stdout.flush()
 
 
-def ambtotal(log):  
+def ambtotal(log):
     f = open(log)
     results = f.read()
     f.close()
-    fit = results.count('yes')
+    return results.count('yes')
 
-    gtot = sum(1 for line in open(log))
-    if fit == gtot:
-        sys.stderr.write("Found all ambiguities! Quitting...")
-        sys.exit(0)
+
+def fitness(log, foundall=False):
+    """ fitness -> number of ambiguities found
+        optionally, we can stop if all grammars are ambiguous (and detected)
+    """
+    fit = ambtotal(log)
+    if foundall:
+        gtot = sum(1 for line in open(log))
+        if fit == gtot:
+            sys.stderr.write("Found all ambiguities! Quitting...")
+            sys.exit(0)
 
     return fit
 
@@ -31,12 +38,71 @@ def runtool(cmd):
         sys.stderr.write("cmd: %s failed!" % (cmdstr))
         sys.exit(1)
 
-def mean(val):
-    return (sum(val) * 1.0)/len(val)
+
+def mean(l):
+    if len(l) > 0:
+        return (sum(l) * 1.0)/len(l)
+
+    return None
 
 
-def stddev(val):
-    m = mean(val)
-    variance = [((v - m)**2) for v in val]
-    return math.sqrt(mean(variance))
+def stddev(l):
+    if len(l) > 0:
+        m = mean(l)
+        variance = [((v - m)**2) for v in l]
+        return math.sqrt(mean(variance))
+
+    return None
+
+
+def neighbour(vals, neigh1, neigh2, neigh3):
+    if len(vals) > 3:
+        recent3 = vals[-3:]
+        recent10 = vals[-10:]
+        recent3vals,recent10vals = [],[]
+        for k,v in recent3:
+            recent3vals.append(v)
+
+        for k,v in recent10:
+            recent10vals.append(v)
+
+        sdev3 = stddev(recent3vals)
+        sdev10 = stddev(recent10vals)
+
+        sys.stderr.write("recent10vals: %s, sdev3: %s, sdev10: %s \n" \
+                         % (recent10vals, sdev3, sdev10))
+        if sdev3 < 3:
+            #if sdev10 < 3:
+            #    return neigh3
+            return neigh2
+
+    return neigh1
+
+
+def keep_running(vals):
+    """ if the last 3 values are less then max, then stop """
+    if len(vals) > 3:
+        _, _f1 = vals[-1]
+        _, _f2 = vals[-2]
+        _, _f3 = vals[-3]
+
+        fitvals = [f for k,f in vals]
+        maxfit = max(fitvals)
+
+        if (_f1 < maxfit) and (_f2 < maxfit) and (_f3 < maxfit):
+            print "\n** Reached (local) maxima! **\n"
+            fitkeys = []
+            for k,f in vals:
+                if f == maxfit:
+                    fitkeys.append(k)
+
+            for k,f in vals:
+                print "(%s,%s)" % (k,f)
+
+            msg = "Options %s found %s ambiguities **\n" % (fitkeys,maxfit)
+            sys.stderr.write(msg)
+            sys.exit(0)
+
+    return True
+
 
--- metasearch/MetasearchAmber.py
+++ metasearch/MetasearchAmber.py
@@ -6,11 +6,12 @@ import math
 import MetaUtils
 
 TIMELIMIT = 10
-EXAMPLES_INCREMENT = 1.1
-EXAMPLES_INCREMENT_LVAL = 1.3
-EXAMPLES_INCREMENT_HVAL = 2.0
-LENGTH_LVAL = 3
-LENGTH_HVAL = 10
+EX_STEP1 = 1.05
+EX_STEP2 = 1.1
+EX_STEP3 = 1.2
+LEN_STEP1 = 1
+LEN_STEP2 = 3
+LEN_STEP3 = 5
 
 class Hillclimb:
 
@@ -22,159 +23,74 @@ class Hillclimb:
         self.ellipsis = ellipsis
         self.timelimit = timelimit
         self.k_values = []
+        self.logd = "%s/results/amber/%s/%ss" % (expdir, gset, self.timelimit)
 
         amberx =  "%s/run_Amber.sh" % (self.expdir)
-        self.cmd = [amberx,"-g",self.gset,"-t",str(self.timelimit)]
+        self.cmd = [amberx, "-g", self.gset, "-t", str(self.timelimit)]
+
         if self.ellipsis:
             self.cmd.append("-e")
+            self.logd += "_ellipsis"
 
         if self.examples is not None:
-            self.logdirprefix = "%ss_examples" % (self.timelimit)
-            self.run_examples()
+            self.switch = "-n"
+            self.initval = self.examples
+            self.logd += "_examples"
         else:
-            self.logdirprefix = "%ss_length" % (self.timelimit)
-            self.run_length()
+            self.switch = "-l"
+            self.initval = self.length
+            self.logd += "_length"
+
+        self.run()
 
 
-    def fitness(self, optval):
-        """ fitness -> number of ambiguities found """
-        amberlogdir = "%s_%s" % (self.logdirprefix, optval)
-
-        if self.ellipsis:
-            amberlogdir = "%s_ellipsis" % (amberlogdir)
-
-        log =  "%s/results/%s/%s/%s/log" % (self.expdir, "amber", self.gset, amberlogdir) 
-        return MetaUtils.ambtotal(log)
-
-
-    def neighbour(self, neigh, sdev3neigh, sdev10neigh):
-        if len(self.k_values) < 3:
-            sys.stderr.write("recent: %s" % self.k_values)
-            return  neigh
-
-        recent3 = self.k_values[-3:]
-        recent10 = self.k_values[-10:]
-        recent3vals,recent10vals = [],[]
-        for k,v in recent3:
-            recent3vals.append(v)
-        
-        for k,v in recent10:
-            recent10vals.append(v)
-        
-        sdev3 = MetaUtils.stddev(recent3vals)
-        sdev10 = MetaUtils.stddev(recent10vals)
-        
-        sys.stderr.write("recent10vals: %s, sdev3: %s, sdev10: %s \n" % (recent10vals,sdev3,sdev10))
-        if sdev3 < 3:
-            return sdev3neigh
-            #if sdev10 < 3:
-            #    return sdev10neigh
-            #else:
-            #    return sdev3neigh
+    def neighbour(self, v):
+        if self.examples is not None:
+            return MetaUtils.neighbour(self.k_values,
+                                       int(math.ceil(v * EX_STEP1)),
+                                       int(math.ceil(v * EX_STEP2)),
+                                       int(math.ceil(v * EX_STEP3)))
         else:
-            return neigh
+            return MetaUtils.neighbour(self.k_values,
+                                       v+LEN_STEP1,
+                                       v+LEN_STEP2,
+                                       v+LEN_STEP3)
 
-
-    def neighbourlen(self, l):
-        return self.neighbour(l+1, l+3, l+10)
-        
-
-    def neighbourex(self, n):
-        return self.neighbour(int(math.ceil(n * EXAMPLES_INCREMENT)), 
-                  int(math.ceil(n * EXAMPLES_INCREMENT_LVAL)), 
-                  int(math.ceil(n * EXAMPLES_INCREMENT_HVAL))) 
-
-
-    def keep_running(self):
-        """ if we see a downward trend for the last 3 values, stop """
-        if len(self.k_values) > 3:
-            _, fit = self.k_values[-1]
-            _, _fit = self.k_values[-2]
-            _, __fit = self.k_values[-3]
-            _, ___fit = self.k_values[-4]
-            if (fit <= _fit) and (_fit <= __fit) and (__fit <= ___fit):
-                return False
-
-        return True
-
-
-    def finish(self):
-        fitvals = [f for k,f in self.k_values]
-        fitkeys = []
-        for k,f in self.k_values:
-            if f == max(fitvals):
-                fitkeys.append(k)
-
-        print "\n** Reached (local) maxima! **\n"
-        for k,f in self.k_values:
-            print "(%s,%s)" % (k,f)
-
-        msg = "Options %s found %s ambiguities **\n" % (fitkeys,max(fitvals))
-        sys.stderr.write(msg)
-        sys.exit(0) 
-
-
-    def run_examples(self):
-        currex = self.examples
-        amberoptcmd = ["-n",str(currex)]
+    def run(self):
+        currval = self.initval
+        amberoptcmd = [self.switch, str(currval)]
         MetaUtils.runtool(self.cmd + amberoptcmd)
-        currfit = self.fitness(currex)
-        self.k_values.append((currex,currfit))
+        currfit = MetaUtils.fitness("%s_%s/log" % (self.logd, currval))
+        self.k_values.append((currval,currfit))
 
         while True:
-            sys.stderr.write("\n==> current examples count: %s, fitness: %s\n" % (str(currex),str(currfit)))
-            neighex = self.neighbourex(currex)
-            sys.stderr.write("neighex: %s" % (str(neighex)))
-            amberoptcmd = ["-n",str(neighex)]
+            sys.stderr.write("\n==> current value: %s, fitness: %s\n" \
+                             % (str(currval),str(currfit)))
+            neighval = self.neighbour(currval)
+            sys.stderr.write("neighval: %s" % (str(neighval)))
+            amberoptcmd = [self.switch, str(neighval)]
             MetaUtils.runtool(self.cmd + amberoptcmd)
-            newfit = self.fitness(neighex)
-            self.k_values.append((neighex,newfit))            
-            
+            newfit = MetaUtils.fitness("%s_%s/log" % (self.logd, neighval))
             sys.stderr.write("new fitness: %s " % str(newfit))
+            self.k_values.append((neighval, newfit)
 
-            if self.keep_running():
-                currex = neighex
+            if MetaUtils.keep_running(self.k_values):
+                currval = neighval
                 currfit = newfit
-            else:
-                self.finish()
 
-
-    def run_length(self):
-        currlen = self.length
-        amberoptcmd = ["-l",str(currlen)]
-        MetaUtils.runtool(self.cmd + amberoptcmd)
-        currfit = self.fitness(currlen)
-        self.k_values.append((currlen,currfit))
-
-        while True:
-            sys.stderr.write("\n==> current length: %s, fitness: %s\n" % (str(currlen),str(currfit)))
-            neighlen = self.neighbourlen(currlen)
-            amberoptcmd = ["-l",str(neighlen)]
-            MetaUtils.runtool(self.cmd + amberoptcmd)
-            newfit = self.fitness(neighlen)
-            self.k_values.append((neighlen,newfit))            
-
-            sys.stderr.write("new fitness: %s " % str(newfit))
-
-            if self.keep_running():
-                currlen = neighlen
-                currfit = newfit
-            else:
-                self.finish()
 
 
 def usage(msg=None):
     if msg is not None:
         sys.stderr.write(msg)
-        
+
     sys.stderr.write("Metasearch.py -x <experiment directory> " \
     "-g <grammar set> -n <number of examples> -l <initial length -e>\n")
     sys.exit(1)
-    
 
 
-if __name__ == "__main__": 
-    opts, args = getopt.getopt(sys.argv[1 : ], "x:g:n:l:e")   
+if __name__ == "__main__":
+    opts, args = getopt.getopt(sys.argv[1 : ], "x:g:n:l:e")
     expdir = None
     gset = None
     length = None
@@ -191,8 +107,8 @@ if __name__ == "__main__":
             length = int(opt[1])
         elif opt[0] == "-e":
             ellipsis = True
-        else: 
-            usage("Unknown argument '%s'" % opt[0])   
+        else:
+            usage("Unknown argument '%s'" % opt[0])
 
     if (expdir is None) or (gset is None):
         usage()
@@ -202,4 +118,4 @@ if __name__ == "__main__":
 
     print opts, args
     Hillclimb(expdir, gset, examples, length, ellipsis, TIMELIMIT)
-    
+
