26609b93e3f2567970ee09d6830f82fa0e4bc334
krish <krish.vasudevan@polestarglobal.com>
2015-05-07 08:55:11 +0100

    Avoid duplication. Use the grammar stats functions from ValidGrammar program.

--- misc/GrammarInfo.py
+++ misc/GrammarInfo.py
@@ -20,128 +20,55 @@
 # IN THE SOFTWARE.
 
 
-import sys
+import os, sys
 import sets
 import CFG, Lexer
+import ValidGrammar
 
-def altsInfo(cfg, size):
-    _total, _empty = 0,0
-    _n_size = 0
-    _max_alt_len = 0
+
+def alts_info(cfg, tlen):
+    total, empty = 0,0
+    long_alts = 0
+    longest = 0
+    nonterms, terms = 0,0
     for rule in cfg.rules:
-        _total += len(rule.seqs)
-        for ind,seq in enumerate(rule.seqs):
-            _len = len(seq)
-            if _len == 0:
-                _empty += 1
+        total += len(rule.seqs)
+        for ind, seq in enumerate(rule.seqs):
+            l = len(seq)
+            if l == 0:
+                empty += 1
                 
-            if _len > size:
-                _n_size += 1
+            if l > tlen:
+                long_alts += 1
                 
-            if _len > _max_alt_len:
-                print "seq: " , seq
-                _max_alt_len = _len
-                
-    return _total, _empty, _n_size, _max_alt_len
+            if l > longest:
+                longest = l
 
-
-def symbolInfo(cfg):
-    _nt_pts,_t_pts = 0,0
-    for rule in cfg.rules:
         for seq in rule.seqs:
             for e in seq:
                 if isinstance(e, CFG.Non_Term_Ref):
-                    _nt_pts+=1 
+                    nonterms += 1
                 else:
-                    _t_pts+=1
-                    
-    return _nt_pts, _t_pts
+                    terms += 1
+
+    return total, empty, long_alts, longest, nonterms, terms
 
 
-
-def removeTerms(cfg):
-    _rules, _indices, _r_remove = {}, {}, []
-    for rule in cfg.rules:
-        rule_seqs = []
-        for ind,seq in enumerate(rule.seqs):
-            _seq = []
-            for e in seq:
-                if isinstance(e, CFG.Non_Term_Ref):
-                    _seq.append(e.name)
-
-            if len(_seq) == 0:
-                if not _indices.__contains__(rule.name):
-                    _indices[rule.name] = ind
-                    _r_remove.append(rule.name)
-            rule_seqs.append(_seq)
-        if not _indices.__contains__(rule.name):
-            _rules[rule.name] = rule_seqs
-            
-    return _rules, _indices, _r_remove
-    
-    
-
-def removeMatchedRuleRefs(rules, indices):
-    _r_remove = []
-    for key in rules.keys():
-        rule_seqs = []
-        for ind,seq in enumerate(rules[key]):
-            _seq = []
-            for e in seq:
-                if e not in indices.keys():
-                    _seq.append(e)
-            if len(_seq)== 0:
-                indices[key] = ind
-                _r_remove.append(key)
-                break
-            rule_seqs.append(_seq)
-                
-        rules[key] = rule_seqs
-        
-    return _r_remove
-       
-       
-       
-def cyclicInfo(cfg,lex):
-    rules, indices, r_remove = removeTerms(cfg)
-    
-    if len(r_remove) > 0:
-        while True:
-            r_remove = removeMatchedRuleRefs(rules,indices)
-            if len(r_remove) == 0:
-                break
-                
-            for _key in r_remove:
-                del rules[_key]
-        
-    cyclic_rules = []
-    for key in rules.keys():
-        if key not in indices.keys():
-            cyclic_rules.append("%s : %s" % (key, rules[key]))
-
-    return cyclic_rules
-
-       
 
 if __name__ == "__main__":
 
     gp = sys.argv[1]
     lp = sys.argv[2]
-    alt_size = sys.argv[3]
+    lalt = int(sys.argv[3])
     lex = Lexer.parse(open(lp, "r").read())
     cfg = CFG.parse(lex, open(gp, "r").read())
-    print cfg
-    print "--"
-    print lex
-    cyclic_rules = cyclicInfo(cfg,lex)
-    total_alts, empty_alts, n_alts_size, max_alt_len = altsInfo(cfg, int(alt_size))
-    nt_pts,t_pts = symbolInfo(cfg)
+    total, empty, long_alts, longest, nonterms, terms = alts_info(cfg, lalt)
+    cycles = ValidGrammar.cyclicInfo(cfg, lex)
     
-    print "no_rules: " , len(cfg.rules)
-    print "empty/total alts: %s/%s" % (str(empty_alts),str(total_alts))
-    print "alt_size_greater > %s: %s" % (alt_size,str(n_alts_size))
-    print "max alt length: %s" % (str(max_alt_len))
-    print "nonterminal_pts: %i,%f" % (nt_pts,(nt_pts*1.0/(nt_pts+t_pts)))
-    print "terminal_pts: %i,%f" % (t_pts,(t_pts*1.0/(nt_pts+t_pts)))     
-    print "nonterminating_rules: %s" % (" \n ".join(cyclic_rules))
+    headers =  "rules, alts, empty, empty(%), long alts, long alts(%),\
+                longest, nonterms, terms, cycles"
+    print headers
+    print "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % \
+          (len(cfg.rules), total, empty, ((empty * 1.0)/total),
+           long_alts, ((long_alts * 1.0)/total) , longest, nonterms, terms, cycles)
     
