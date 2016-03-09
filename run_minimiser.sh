2faf2f756c090b7ed4732aea10cdc884dced6853
krish <krish.vasudevan@polestarglobal.com>
2016-03-09 17:26:44 +0000

    Script for running minimiser experiment

--- run_minimiser.sh
+++ run_minimiser.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
+# modify the below settings before launch
+ambiminp="${HOME}/codespace/sinbad/src/AmbiMin.py"
 bolzdir="${HOME}/codespace/experiment/grammars/boltzcfg"
+langdir="${HOME}/codespace/experiment/grammars/lang/acc"
 mutdir="${HOME}/codespace/experiment/grammars/mutlang/acc"
 lexdir="${HOME}/codespace/experiment/grammars/lex"
 backend=""
-depth=
+depth=""
 weight=""
 minimiser=""
 gset=""
 nmin=""
 tmin=""
-tout=""
-savecfg=""
+fltr=""
+outf=""
+tout="60"
 
-set -- $(getopt m:g:n:t:b:w:d:T:s "$@")
+export ACCENT_DIR=$HOME/codespace/accent
+
+set -- $(getopt b:m:g:n:t:d:w:f:o: "$@")
 
 usage(){
     echo "$0 
-            -m <minimiser> 
             -b <backend> 
             -d <depth> 
-	    -w <(applicable for dynamic4) weight> 
+            -w <weight> 
+            -m <minimiser> 
             -g <grammar set> 
-            -T <total time for minimisation>
-	    -n <(applicable for min1) no of minimisations> 
-	    -t <(applicable for min2) search time when a rule is removed>
-	    -c <(applicable for min1a) reset counter: no of times to keep 
-	        running minimiser in the hope there will be further reduction
-	    -s <save the minimised cfg .m extension>
-	 "
+            -n <no of minimisations (for min1)> 
+            -t <time to search for ambiguity (for min2)>
+            -f <filter to apply (for min4)>
+            -o <output format for minimised grammar (for min4)>"
     exit 1
 }
 
@@ -36,16 +39,15 @@ usage(){
 while [ $# -gt 0 ]
 do
     case "$1" in 
-     -m) minimiser=$2 ; shift;;
      -b) backend=$2 ; shift;;
      -d) depth=$2 ; shift;;
      -w) weight=$2 ; shift;;
+     -m) minimiser=$2 ; shift;;
      -g) gset=$2 ; shift;;
      -n) nmin=$2 ; shift;;
-     -c) cmin=$2 ; shift;;
      -t) tmin=$2 ; shift;;
-     -T) tout=$2 ; shift;;
-     -s) savecfg="-s" ; shift;;
+     -f) fltr=$2 ; shift;;
+     -o) outf=$2 ; shift;;
     (--) shift; break;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; usage; exit 1;;
      (*) break;;  
@@ -53,68 +55,113 @@ do
     shift
 done
 
-# checks
 [ -z "$backend" ] && usage
-[ -z "$depth" ] && usage
+[ -d "$depth" ] && usage
 [ -z "$minimiser" ] && usage
 [ -z "$gset" ] && usage
-[ -z "$tout" ] && usage
-[ "$minimiser" == "min1" ]   && [ -z "$nmin" ] && usage
-[ "$minimiser" == "min2" ]   && [ -z "$tmin" ] && usage
-[ "$minimiser" == "min1a" ]  && [ -z "$cmin" ] && usage
-[ "$backend" == "dynamic4" ] && [ -z "$weight" ] && usage
 
-# build cmd
-cmd="timeout ${tout}s python AmbiMin.py -b $bend -d $d -m $minimiser"
-[ "$minimiser" == "min1" ]   && cmd="$cmd -n $nmin"
-[ "$minimiser" == "min1a" ]  && cmd="$cmd -c $cmin"
-[ "$minimiser" == "min2" ]   && cmd="$cmd -t $tmin"
-[ "$backend" == "dynamic4" ] && cmd="$cmd -w $weight"
-[ ! -z "$savecfg" ] && cmd="$cmd -s"
-
+cmd="timeout ${tout}s python $ambiminp -b $backend -w $weight -d $depth -m $minimiser"
 logd="$(pwd)/log/$minimiser/$gset"
 [ ! -d $logd ] && mkdir -p $logd
 
-# TODO: download and setup up accent in workdir
-setup_accent() {
-  export ACCENT_DIR=$HOME/codespace/accent
-}
+case "$minimiser" in
+    min1) 
+      if [ -z "$nmin" ]; then
+        echo "min1 requires -n <no of minimisations>"
+        usage
+      fi
+      cmd="$cmd -n $nmin -s"
+    ;;
+    min1a) 
+      # TO DO
+    ;;
+    min2|minad) 
+      if [ -z "$tmin" ]; then
+        echo "min{2|ad} requires -t <time to search for ambiguity>"
+        usage
+      fi
+      cmd="$cmd -t $tmin -s"
+    ;;
+    min3) 
+      if [ -z "$nmin" ]; then
+        echo "min3 requires -n <no of minimisations>"
+        usage
+      fi
+      if [ -z "$tmin" ]; then
+        echo "min3 requires -t <time to search for ambiguity>"
+        usage
+      fi
+      cmd="$cmd -n $nmin -t $tmin -s"
+    ;;
+    min4) 
+      if [ -z "$nmin" ]; then
+        echo "min4 requires -n <no of minimisations>"
+        usage
+      fi
+      if [ -z "$tmin" ]; then
+        echo "min4 requires -t <time to search for ambiguity>"
+        usage
+      fi
+      if [ -z "$fltr" ]; then
+        echo "min4 requires -f <filter to apply for ambidexter>"
+        usage
+      fi
+      if [ -z "$outf" ]; then
+        echo "min4 requires -t <output format for ambidexter>"
+        usage
+      fi
+      cmd="$cmd -n $nmin -t $tmin -f $fltr -o $outf -s"
+    ;;
+esac
 
 boltz() {
-  for g in $(seq 10 65); do
+  for g in $(seq 10 75); do
     for i in $(seq 10); do 
       gacc="$bolzdir/$g/${i}.acc"
       lex="$bolzdir/$g/lex"
-      $cmd $gacc $lex > $logd/${g}_${i}.log 
-      gzip $logd/${g}_${i}.log
-      sizes=$(zgrep ^stats $logd/${g}_${i}.log.gz | cut -d: -f3) 
-      echo "$g/$i,$(echo $sizes | sed -e 's/ /,/g')" 
+      log="/tmp/${g}_${i}.${minimiser}"
+      stats=$($cmd -l $log $gacc $lex)
+      echo "$g/$i,$(cat $log)"
+      rm $log
     done
   done
 }
 
+lang() {
+  for l in Pascal SQL Java C; do 
+    for i in $(seq 5); do
+      gacc="$langdir/${l}.${i}.acc"
+      lex="$lexdir/${l}.lex"
+      log="/tmp/${l}_${i}.${minimiser}"
+      stats=$($cmd -l $log $gacc $lex)
+      echo "${l}.$i,$(cat $log)"
+      rm $log
+    done
+   done
+}
+
 mutlang() {
-  echo "=> $m"
-  for t in empty add mutate delete switchadj switchany; do 
+  for t in empty add mutate delete switch; do 
     mkdir -p $logd/$t
     for l in Pascal SQL Java C CSS; do 
       for i in $(seq 10); do
         gacc="$mutdir/$t/${l}/${l}.0_${i}.acc"
         lex="$lexdir/${l}.lex"
-        $cmd $gacc $lex > $logd/$t/${l}_${i}.log 
-        gzip $logd/$t/${l}_${i}.log
-        sizes=$(zgrep ^stats $logd/$t/${l}_${i}.log.gz | cut -d: -f3) 
-        echo "$t/${l}_$i,$(echo $sizes | sed -e 's/ /,/g')"
+        log="/tmp/${t}_${l}_${i}.${minimiser}"
+        stats=$($cmd -l $log $gacc $lex)
+        echo "$t/${l}.$i,$(cat $log)"
+        rm $log
       done
      done
   done
 }
 
-setup_accent
-
 case "$gset" in
     boltz)
       boltz | tee boltz.${minimiser}.csv
+      ;;
+    lang)
+      lang | tee lang.${minimiser}.csv
       ;;
     mutlang)
       mutlang | tee mutlang.${minimiser}.csv
