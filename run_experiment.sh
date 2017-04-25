8dca90f2405d22263d925de64fd5f79cb066381b
Naveneetha Vasudevan <naveneetha@yahoo.com>
2017-04-25 13:46:16 +0100

    Use Pascal grammar for travis but use use -k for acla

--- run_experiment.sh
+++ run_experiment.sh
@@ -57,7 +57,7 @@ $scriptsdir/download_grammars.sh
 # for travis: run the tools on test grammars
 if [ "$exp" == "travis" ]; then
   for g in test lang; do
-    $scriptsdir/run_acla.sh -g $g -t 10
+    $scriptsdir/run_acla.sh -g $g -t 10 -k
     [ $(grep -c yes $resultsdir/acla/$g/10s/log) == 1 ] || exit 1
 
     $scriptsdir/run_amber.sh -g $g -t 10 -l 10
